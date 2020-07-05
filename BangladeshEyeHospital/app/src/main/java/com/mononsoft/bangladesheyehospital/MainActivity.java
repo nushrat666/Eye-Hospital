4b2a59ab305ad2e03261876b6b2cc97c9cbf7ee6
Souls <mdsazzad05@gmail.com>
2020-07-05 15:05:49 +0600

    menu

--- BangladeshEyeHospital/app/src/main/java/com/mononsoft/bangladesheyehospital/MainActivity.java
+++ BangladeshEyeHospital/app/src/main/java/com/mononsoft/bangladesheyehospital/MainActivity.java
@@ -1,10 +1,17 @@
 package com.mononsoft.bangladesheyehospital;
 
+//activity-menu-MenuDrawerNews
+
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActionBar actionBar;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
