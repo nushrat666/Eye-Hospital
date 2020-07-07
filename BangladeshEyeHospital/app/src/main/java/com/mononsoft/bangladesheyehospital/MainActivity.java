41c71cd61bbefbb9eab8704545f3b647bc53d74a
Souls <mdsazzad05@gmail.com>
2020-07-07 15:17:06 +0600

    Home Page

--- BangladeshEyeHospital/app/src/main/java/com/mononsoft/bangladesheyehospital/MainActivity.java
+++ BangladeshEyeHospital/app/src/main/java/com/mononsoft/bangladesheyehospital/MainActivity.java
@@ -1,21 +1,108 @@
 package com.mononsoft.bangladesheyehospital;
 
-//activity-menu-MenuDrawerNews
-
-import androidx.appcompat.app.ActionBar;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
 
-    private ActionBar actionBar;
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    private static final String TAG = "MainActivity";
+
+    private DrawerLayout drawer;
+    private NavigationView navigationView;
     private Toolbar toolbar;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initViews();
+        setSupportActionBar(toolbar);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle (this,drawer,toolbar,
+                R.string.drawer_open,R.string.drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+        navigationView.setNavigationItemSelectedListener(this);
+
+
+        FragmentTransaction transaction=getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragment_container,new MainFragment());
+        transaction.commit();
+    }
+
+    private void initViews() {
+
+        Log.d(TAG, "initViews: started");
+        drawer = (DrawerLayout) findViewById(R.id.drawer);
+        navigationView = (NavigationView) findViewById(R.id.navigationDrawer);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()){
+            case R.id.nav_home:
+                Toast.makeText(this, "Home Selected", Toast.LENGTH_SHORT).show();
+                //TODO: right the proper logic for different situations
+                break;
+
+            case R.id.nav_login:
+                Toast.makeText(this, "Home Selected", Toast.LENGTH_SHORT).show();
+                //TODO: right the proper logic for different situations
+                break;
+
+            case R.id.nav_order:
+                Toast.makeText(this, "Order Selected", Toast.LENGTH_SHORT).show();
+                //TODO: right the proper logic for different situations
+                break;
+
+            case R.id.nav_user_request:
+                Toast.makeText(this, "User Request Selected", Toast.LENGTH_SHORT).show();
+                //TODO: right the proper logic for different situations
+                break;
+
+            case R.id.nav_doctors_profile:
+                Toast.makeText(this, "Doctors Profile Selected", Toast.LENGTH_SHORT).show();
+                //TODO: right the proper logic for different situations
+                break;
+
+            case R.id.nav_services:
+                Toast.makeText(this, "Services Selected", Toast.LENGTH_SHORT).show();
+                //TODO: right the proper logic for different situations
+                break;
+            case R.id.nav_authority:
+                Toast.makeText(this, "Authority Selected", Toast.LENGTH_SHORT).show();
+                //TODO: right the proper logic for different situations
+                break;
+            case R.id.nav_branches:
+                Toast.makeText(this, "Branches Selected", Toast.LENGTH_SHORT).show();
+                //TODO: right the proper logic for different situations
+                break;
+            case R.id.nav_about:
+                Toast.makeText(this, "About Selected", Toast.LENGTH_SHORT).show();
+                //TODO: right the proper logic for different situations
+                break;
+            case R.id.nav_admin:
+                Toast.makeText(this, "Admin Selected", Toast.LENGTH_SHORT).show();
+                //TODO: right the proper logic for different situations
+                break;
+
+
+            default:
+
+                break;
+        }
+
+        return false;
     }
 }
---
+++ BangladeshEyeHospital/app/src/main/java/com/mononsoft/bangladesheyehospital/MainFragment.java
@@ -0,0 +1,28 @@
+package com.mononsoft.bangladesheyehospital;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+/**
+ * Created by Md Mehadi Hasan on 7/7/2020.
+ * Email: bdmehadih@gmail.com
+ * Website: www.mehadih.me
+ * Project Name: Bangladesh Eye Hospital
+ */
+public class MainFragment extends Fragment {
+
+    private static final String TAG = "MainFragment";
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        return view;
+    }
+}
