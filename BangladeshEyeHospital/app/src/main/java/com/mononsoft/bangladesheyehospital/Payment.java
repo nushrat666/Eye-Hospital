1e53d786d9085d37eae4a38745a88cfa9f1fbc3c
Souls <mdsazzad05@gmail.com>
2020-07-16 14:57:34 +0600

    payment

--- BangladeshEyeHospital/app/src/main/java/com/mononsoft/bangladesheyehospital/Payment.java
+++ BangladeshEyeHospital/app/src/main/java/com/mononsoft/bangladesheyehospital/Payment.java
@@ -3,28 +3,21 @@ package com.mononsoft.bangladesheyehospital;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
-public class ServiceActivity extends AppCompatActivity {
+public class Payment extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_service);
+        setContentView(R.layout.activity_payment);
         initToolbar();
     }
     private void initToolbar() {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        getSupportActionBar().setTitle("Services");
+        getSupportActionBar().setTitle("Payment");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         Tools.setSystemBarColor(this, R.color.colorPrimary);
-    }
-
-    public void payment(View view) {
-        Intent intent = new Intent(ServiceActivity.this,Payment.class);
-        startActivity(intent);
     }
 }
